a2a01997048d82e1c0956c87522bfb82a2f583e5
GurovaAnya <anna30850@gmail.com>
2021-07-17 18:28:17 +0500

    докер

--- ECommerceApi/Startup.cs
+++ ECommerceApi/Startup.cs
@@ -52,13 +52,14 @@ namespace ECommerceApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ECommerceApi v1"));
-            }
-
+            // if (env.IsDevelopment())
+            // {
+            //    
+            // }
+             app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ECommerceApi v1"));
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
---
+++ ECommerceApi/initDb.sql
@@ -0,0 +1,20 @@
+create database if not exists ecommercedb ;
+
+use ecommercedb;
+
+create table item
+(
+    id int auto_increment
+        primary key,
+    sku varchar(255) not null,
+    name varchar(255) not null,
+    type varchar(255) not null,
+    price decimal not null,
+    constraint item_sku_uindex
+        unique (sku)
+);
+
+INSERT INTO item (sku, name, type, price) VALUES ('AA/01/001', 'iPhone 14', 'Phone', 999);
+INSERT INTO item (sku, name, type, price) VALUES ('AA/01/002', 'Samsung Galaxy Note 50', 'Phone', 998);
+INSERT INTO item (sku, name, type, price) VALUES ('BB/29/324', 'iPad 10', 'Tablet', 1499);
+INSERT INTO item (sku, name, type, price) VALUES ('AB/93/485', 'miBand', 'Smart Watch', 9);
